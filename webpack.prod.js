89bf0e23851f5317e5f2c149c19013b15bd767e8
Vulpie <wojtas_f@outlook.com>
2020-12-17 14:25:31 +0100

    feat: add autoprefixer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
-const path = require("path");
-const config = require("./webpack.config");
-const merge = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin"); // this is preinstalled by webpack to minify js
+const path = require("path")
+const config = require("./webpack.config")
+const merge = require("webpack-merge")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin") // this is preinstalled by webpack to minify js
 
 module.exports = merge(config, {
   mode: "production",
@@ -27,6 +27,9 @@ module.exports = merge(config, {
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader", //2.Turns css into js
+          {
+            loader: "postcss-loader",
+          },
           "sass-loader", // 1. Turns sass into css
         ], //this works in reverse order
       },
@@ -36,10 +39,11 @@ module.exports = merge(config, {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ["@wordpress/default"],
+            plugins: ["babel-plugin-loop-optimizer"],
           },
         },
       },
     ],
   },
-});
+})
