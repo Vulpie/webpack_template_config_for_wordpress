8f8b8385b4b5b52a201ad085f48926de4f091c42
Vulpie <wojtas_f@outlook.com>
2020-12-17 09:08:25 +0100

    feat: add babel and change config for wordpress

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,17 @@
-
 module.exports = {
-    entry: './src/main/js/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(svg|png|jpg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[hash].[ext]",
-                        outputPath: "imgs"
-                    }
-                }
-            }
-        ]
-    },
-
-}
+  entry: "./src/main/js/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs",
+          },
+        },
+      },
+    ],
+  },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,44 @@
-const path = require("path")
-const config = require("./webpack.config")
-const merge = require("webpack-merge")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const config = require("./webpack.config");
+const merge = require("webpack-merge");
 
 module.exports = merge(config, {
-    mode: "development",
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "build")
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/templates/index.html"
-        })
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2.Turns css into js
+          "sass-loader", // 1. Turns sass into css
+        ], //this works in reverse order
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader', //3. Inject styles into DOM
-                    'css-loader', //2.Turns css into js
-                    'sass-loader' // 1. Turns sass into css
-                ] //this works in reverse order
-            },
-        ]
-    }
-})
+  },
+});
+
+module.exports = merge(config, {
+  mode: "development",
+  devtool: "none",
+  output: {
+    filename: "[name].[contentHash].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader", //2.Turns css into js
+          "sass-loader", // 1. Turns sass into css
+        ], //this works in reverse order
+      },
+    ],
+  },
+});
